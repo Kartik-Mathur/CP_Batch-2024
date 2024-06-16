4ed7dbab9342c968130fb4a561307f4379c1610d
Kartik <kartik.mathur10@gmail.com>
2024-06-16 12:48:03 +0530

    Programming Concepts

---
+++ L21-DP-Bitmask/1_Matching.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define 	ll long long int
+#define 	new(a,n) ll*a = new ll[n]
+#define 	new1(a,n,m) ll**a = new ll*[n]; for(ll i = 0 ; i < n ; i++) a[i] = new ll[m];
+#define 	mp make_pair
+#define 	pb push_back
+#define 	fi first
+#define 	pii pair<ll,ll>
+#define 	si second
+#define 	fastIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define 	F(a,n) for(int i=0;i<n;i++){cin>>a[i];}
+#define 	F1(a,n) for(int i=1;i<=n;i++){cin>>a[i];}
+#define 	P(a,n) for(int i=0;i<n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define 	P1(a,n) for(int i=1;i<=n;i++){cout<<a[i]<<' ';}cout<<endl;
+#define 	NF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cin>>a[i][j];}}
+#define 	NF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cin>>a[i][j];}}
+#define 	PNF(a,n,m) for(int i=0;i<n;i++){for(int j=0;j<m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define 	PNF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
+#define 	init(a,n,v) for(int i = 0 ; i <n ; i++) a[i]=v;
+#define 	init2(a,n,m,v) for(int i=0;i<=n;i++){for(int j=0;j<=m;j++){a[i][j]=v;}}
+#define 	w(t) int t;cin>>t;while(t--)
+#define 	f(n) for(int __i=0;__i<n;__i++)
+#define 	AS 200001
+#define 	mod 1000000007
+ll n;
+ll dp[21][(1 << 21) + 1];
+ll comp[22][22];
+
+ll matching(ll i, ll WM) {
+	// base case
+	if (i == n) {
+		return WM == 0;
+	}
+	if (dp[i][WM] != -1) return dp[i][WM];
+
+	// recursive case
+	ll ans = 0;
+	for (int j = 0; j < n; ++j)
+	{
+		bool available = (WM & (1 << j));
+		if (available and comp[i][j]) {
+			ans = (ans + matching(i + 1, WM ^ (1 << j))) % mod;
+		}
+	}
+	return dp[i][WM] = ans;
+}
+
+void solve() {
+	cin >> n;
+	NF(comp, n, n);
+	ll WM = (1 << n) - 1;
+	memset(dp, -1, sizeof dp);
+
+	cout << matching(0, WM) << endl;
+}
+
+int main() {
+	fastIO
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	solve();
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
